61a2b2082cd72cd57e4559ef78b6a29b4fa492ab
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-07-08 02:02:00 -0400

    settingup routes

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
-const sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 const sequelize = process.env.JAWSDB_URL
-? new sequelize(process.env.JAWSDB_URL)
-: new sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+? new Sequelize(process.env.JAWSDB_URL)
+: new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     dialectOptions: {
