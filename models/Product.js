e0952df2a8fa5ca30d1816487742258d39f8666d
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-07-08 00:35:33 -0400

    setup routes

---
+++ models/Product.js
@@ -0,0 +1,54 @@
+const { Model, DataTypes, INTEGER, STRING} = require('sequelize');
+
+//import our database connection from config.js
+const sequelize = require('../config/connection.js');
+
+class Product extends Model {}
+
+//setup fields and rules for Product model
+
+Product.init(
+    {
+        id: {
+            type: INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+          },
+          product_name: {
+            type: STRING,
+            allowNull: false
+          },
+          price: {
+            type: DECIMAL,
+            allowNull: false,
+            validate: {
+              isDecimal: true
+            }
+          },
+          stock: {
+            type: INTEGER,
+            allowNull: false,
+            defaultValue: 10,
+            validate: {
+              isNumeric:true
+            }
+          },
+          category_id: {
+            type: INTEGER,
+            references: {
+              model: 'category',
+              key: 'id'
+            }
+          }
+        },
+        {
+          sequelize,
+          timestamps: false,
+          freezeTableName: true,
+          underscored: true,
+          modelName: 'product',
+        }
+      );   
+
+      model.exports = Product;
